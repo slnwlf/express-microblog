d096234c4b06de0c64584b9f67eca293ec46d86d
YOUR_GITHUB_USERNAME <dave.sloan@gmail.com>
2015-11-14 15:46:56 -0800

    set up AJAX call to GET all blogposts

--- main.js
+++ main.js
@@ -6,11 +6,25 @@ $(function() {
 	var template = Handlebars.compile(source);
 
 	// array of test data
-	var allBlogposts = [
-	{ title: "The sun goes up, the sun goes down", story: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vel neque recusandae doloribus qui dolorum. Molestiae inventore, corporis aut, impedit excepturi quidem temporibus! Consequuntur nobis labore eaque recusandae molestias illo maiores?"},
-	{ title: "Once upon a time in a galaxy far, far way", story: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vel neque recusandae doloribus qui dolorum. Molestiae inventore, corporis aut, impedit excepturi quidem temporibus! Consequuntur nobis labore eaque recusandae molestias illo maiores?"},
-	];
+	var allBlogposts = [{
+		title: "The sun goes up, the sun goes down",
+		story: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vel neque recusandae doloribus qui dolorum. Molestiae inventore, corporis aut, impedit excepturi quidem temporibus! Consequuntur nobis labore eaque recusandae molestias illo maiores?"
+	}, {
+		title: "Once upon a time in a galaxy far, far way",
+		story: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vel neque recusandae doloribus qui dolorum. Molestiae inventore, corporis aut, impedit excepturi quidem temporibus! Consequuntur nobis labore eaque recusandae molestias illo maiores?"
+	}, ];
 
-	var blogpostsHtml = template({ blogPosts: allBlogposts });
+	// AJAX call to GET all blogposts
+
+	$.get('/api/blogposts', function(data) {
+		allBlogposts = data.blogPosts;
+		var blogpostsHtml = template({ blogPosts: data.blogPosts
+		});
+		$('#blogPosts-list').append(blogpostsHtml);
+	});
+
+	var blogpostsHtml = template({
+		blogPosts: allBlogposts
+	});
 	$('#blogposts-list').append(blogpostsHtml);
 });
