f18a9a39f6b3e66199ae85acda767b6196657c86
YOUR_GITHUB_USERNAME <dave.sloan@gmail.com>
2015-11-14 15:36:49 -0800

    got templating working

--- main.js
+++ main.js
@@ -1,41 +1,16 @@
 $(function() {
 	console.log("Javascript is working!");
 
+	// compile handlebars template
+	var source = $('#microblog-template').html();
+	var template = Handlebars.compile(source);
 
+	// array of test data
+	var allBlogposts = [
+	{ title: "The sun goes up, the sun goes down", story: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vel neque recusandae doloribus qui dolorum. Molestiae inventore, corporis aut, impedit excepturi quidem temporibus! Consequuntur nobis labore eaque recusandae molestias illo maiores?"},
+	{ title: "Once upon a time in a galaxy far, far way", story: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vel neque recusandae doloribus qui dolorum. Molestiae inventore, corporis aut, impedit excepturi quidem temporibus! Consequuntur nobis labore eaque recusandae molestias illo maiores?"},
+	];
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+	var blogpostsHtml = template({ blogPosts: allBlogposts });
+	$('#blogposts-list').append(blogpostsHtml);
 });
