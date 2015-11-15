edb2612df0bdd627790cd514001e6d3e3ec0be90
YOUR_GITHUB_USERNAME <dave.sloan@gmail.com>
2015-11-14 17:32:28 -0800

    everything is working, ready for Mongo

--- public/main.js
+++ public/main.js
@@ -5,14 +5,8 @@ $(function() {
 	var source = $('#microblog-template').html();
 	var template = Handlebars.compile(source);
 
-	// array of test data
-	var allBlogposts = [{
-		title: "The sun goes up, the sun goes down",
-		story: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vel neque recusandae doloribus qui dolorum. Molestiae inventore, corporis aut, impedit excepturi quidem temporibus! Consequuntur nobis labore eaque recusandae molestias illo maiores?"
-	}, {
-		title: "Once upon a time in a galaxy far, far way",
-		story: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vel neque recusandae doloribus qui dolorum. Molestiae inventore, corporis aut, impedit excepturi quidem temporibus! Consequuntur nobis labore eaque recusandae molestias illo maiores?"
-	}, ];
+
+	// API routes
 
 	// AJAX call to GET all blogposts
 
--- server.js
+++ server.js
@@ -2,28 +2,49 @@ var express = require('express'),
 	app = express(),
 	bodyParser = require('body-parser');
 
-	// bodyParser gets data out of forms
+// bodyParser gets data out of forms
 
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({
+	extended: true
+}));
 
-// set hbs as server engine. 
+// set hbs as server view engine. 
 
 // use public forlder for static files
 
-// app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/public'));
 
-app.use(express.static('public'));
+// app.use(express.static('public'));
 
-app.set('view engine','hbs');
+app.set('view engine', 'hbs');
 
-	// Homepage route (temp)
+// Homepage route (temp - pre Mongo)
 
-app.get('/', function (req, res) {
-	res.send('hello world');
+app.get('/', function(req, res) {
+	res.render('index');
+});
+
+// API routes
+
+// test data
+
+// array of test data
+var allBlogposts = [{
+	title: "The sun goes up, the sun goes down",
+	story: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vel neque recusandae doloribus qui dolorum. Molestiae inventore, corporis aut, impedit excepturi quidem temporibus! Consequuntur nobis labore eaque recusandae molestias illo maiores?"
+}, {
+	title: "Once upon a time in a galaxy far, far way",
+	story: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vel neque recusandae doloribus qui dolorum. Molestiae inventore, corporis aut, impedit excepturi quidem temporibus! Consequuntur nobis labore eaque recusandae molestias illo maiores?"
+}, ];
+
+app.get('/api/blogposts', function (req, res) {
+	res.json({
+		blogPosts: allBlogposts
+	});
 });
 
 
 // starting server on localhost: 3000
-app.listen(3000, function () {
+app.listen(3000, function() {
 	console.log('server started');
 });
